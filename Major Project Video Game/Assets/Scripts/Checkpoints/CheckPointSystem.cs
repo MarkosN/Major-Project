e3d577483791f2ef41420fbb6ad18bf782016c14
Markos <12499gr@saeinstitute.edu>
2022-10-22 17:21:31 +0300

    Multiplayer Position Tracker/Checkpoint
    -Completed and developed a new way of how the position of each player on the track will be calculated and presented. I scrapped the old 2 ways and made a new unique way of how they players will know their place (minimized the anxious part of it). Everything works as intended without any bug.
    [It is added on track 3. Next I will add it on track 1 and 2]

---
+++ Major Project Video Game/Assets/Scripts/Checkpoints/CheckPointSystem.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class CheckPointSystem : MonoBehaviour // Scraped
+{
+    public float[] checkpointsDistances;
+
+    public Transform player1;
+    public Transform player2;
+
+    private float firstPos;
+    private float secondPos;
+
+    public GameObject nextCheckpoint;
+
+    public GameObject car1DisplayPosFront;
+    public GameObject car2DisplayPosFront;
+
+    public LapsManagerMultiplayer lapsCount;
+    public LapsManager lapsCount2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextCheckpoint.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkpointsDistances[0] = Vector3.Distance(transform.position, player1.position);
+        checkpointsDistances[1] = Vector3.Distance(transform.position, player2.position);
+
+        Array.Sort(checkpointsDistances);
+
+        firstPos = checkpointsDistances[0];
+        secondPos = checkpointsDistances[1];
+
+        float car1Distance = Vector3.Distance(transform.position, player1.position);
+        float car2Distance = Vector3.Distance(transform.position, player2.position);
+
+        if (car1Distance == firstPos && lapsCount2.lapsNumber >= lapsCount.lapsNumberMultiplayer)
+        {
+            car1DisplayPosFront.GetComponent<TMP_Text>().text = "1st";
+        }
+        if (car1Distance == secondPos && lapsCount2.lapsNumber <= lapsCount.lapsNumberMultiplayer)
+        {
+            car1DisplayPosFront.GetComponent<TMP_Text>().text = "2nd";
+        }
+
+        if (car2Distance == firstPos && lapsCount.lapsNumberMultiplayer >= lapsCount2.lapsNumber)
+        {
+            car2DisplayPosFront.GetComponent<TMP_Text>().text = "1st";
+        }
+        if (car2Distance == secondPos && lapsCount.lapsNumberMultiplayer <= lapsCount2.lapsNumber)
+        {
+            car2DisplayPosFront.GetComponent<TMP_Text>().text = "2nd";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            nextCheckpoint.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
