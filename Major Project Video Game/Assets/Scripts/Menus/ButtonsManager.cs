e999ea21c684d712a037d51986069c5dfcca320f
Markos <12499gr@saeinstitute.edu>
2022-10-28 20:19:05 +0300

    Main Menu 4
    -Complete (the content) of the game modes and credits sections. +Color changes on the main menu's headlines.
    [+minor improvements].

--- Major Project Video Game/Assets/Scripts/Menus/ButtonsManager.cs
+++ Major Project Video Game/Assets/Scripts/Menus/ButtonsManager.cs
@@ -10,6 +10,36 @@ public class ButtonsManager : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    // Tracks (and Modes) Buttons that will give to the players access to them
+    public void PracticeModeTrack()
+    {
+        SceneManager.LoadScene("Track0Practice");
+    }
+    public void TimeTrialsTrack1()
+    {
+        SceneManager.LoadScene("Track1TimeTrials");
+    }
+    public void TimeTrialsTrack2()
+    {
+        SceneManager.LoadScene("Track2TimeTrials");
+    }
+    public void TimeTrialsTrack3()
+    {
+        SceneManager.LoadScene("Track3TimeTrials");
+    }
+    public void MultiplayerTrack1()
+    {
+        SceneManager.LoadScene("Track1Multiplayer");
+    }
+    public void MultiplayerTrack2()
+    {
+        SceneManager.LoadScene("Track2Multiplayer");
+    }
+    public void MultiplayerTrack3()
+    {
+        SceneManager.LoadScene("Track3Multiplayer");
+    }
+
     public void Exit() // Button to Give the Player the Ability to Exit the Game
     {
         Application.Quit();
