7684f90ae64a5c32c7d115037423f439614f1cf6
Markos <12499gr@saeinstitute.edu>
2022-11-06 01:27:34 +0200

    Reset Car System 2
    -Made the final adjustment so the player cannot reset the car continuously and break the game or use it as an unfair advamtage, now the reset the car function has a cooldown of 1 second.

--- Major Project Video Game/Assets/Scripts/Car/CarResetPos.cs
+++ Major Project Video Game/Assets/Scripts/Car/CarResetPos.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CarResetPos : MonoBehaviour // When the car is stuck or can't be moved it will be reset to a new position so the players can keep on going - Player 1
@@ -5,14 +6,25 @@ public class CarResetPos : MonoBehaviour // When the car is stuck or can't be mo
     // Player 1
     public Rigidbody car1; // Car 1
     public GameObject car1ResetPos; // Car's 1 Reset position
+    public bool canReset; // Check if we can reset the car or we should wait X seconds
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1) && canReset == true)
         {
             car1.transform.position = car1ResetPos.transform.position; // Fixed position to reset, reset position will be the same
             car1.velocity = new Vector3(0, 0, 0); // The car will be resetted like it was on the race start
             car1.transform.rotation = Quaternion.Euler(0, 180, 0); // Fixed roation on the car so it can start normally not in the old position that it was on the track
+
+            StartCoroutine(ResetTheCar());
         }
+    }
+
+    IEnumerator ResetTheCar()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canReset = false;
+        yield return new WaitForSeconds(1.0f);
+        canReset = true;
     }
 }
--- Major Project Video Game/Assets/Scripts/Car/CarResetPosV2.cs
+++ Major Project Video Game/Assets/Scripts/Car/CarResetPosV2.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CarResetPosV2 : MonoBehaviour // When the car is stuck or can't be moved it will be reset to a new position so the players can keep on going - Player 2
@@ -5,6 +6,7 @@ public class CarResetPosV2 : MonoBehaviour // When the car is stuck or can't be 
     // Player 2
     public Rigidbody car2; // Car 2
     public GameObject car2ResetPos; // Car's 2 Reset position
+    public bool canReset; // Check if we can reset the car or we should wait X seconds
 
     private void Update()
     {
@@ -13,6 +15,16 @@ public class CarResetPosV2 : MonoBehaviour // When the car is stuck or can't be 
             car2.transform.position = car2ResetPos.transform.position; // Fixed position to reset, reset position will be the same
             car2.velocity = new Vector3(0, 0, 0); // The car will be resetted like it was on the race start
             car2.transform.rotation = Quaternion.Euler(0, 180, 0); // Fixed roation on the car so it can start normally not in the old position that it was on the track
+
+            StartCoroutine(ResetTheCar());
         }
+    }
+
+    IEnumerator ResetTheCar()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canReset = false;
+        yield return new WaitForSeconds(1.0f);
+        canReset = true;
     }
 }
