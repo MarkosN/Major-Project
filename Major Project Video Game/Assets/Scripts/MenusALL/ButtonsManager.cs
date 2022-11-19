bf29ae2249b8914e19d02d0469e8fd56cfcd5d62
Markos <12499gr@saeinstitute.edu>
2022-11-19 02:05:30 +0200

    Final Changes/Fixes 3
    -I made it to reset the times on the laps when the players restart the race or exit it via the main menu (except the ones which are intentionally saved).

--- Major Project Video Game/Assets/Scripts/MenusALL/ButtonsManager.cs
+++ Major Project Video Game/Assets/Scripts/MenusALL/ButtonsManager.cs
@@ -7,6 +7,38 @@ public class ButtonsManager : MonoBehaviour
     {
         Time.timeScale = 1f; // 1f - Normal Time
         SceneManager.LoadScene("LoadingScene");
+
+        // Resetting the times on the laps (except the one which are intentionally saved)
+        //Total Time Counters (Time Trials Mode)
+        TotalTimeCounter.totalMinutesCounter = 0;
+        TotalTimeCounter.totalSecondsCounter = 0;
+        TotalTimeCounter.totalMillisecondsCounter = 0;
+        TotalTimeCounter2.totalMinutesCounter2 = 0;
+        TotalTimeCounter2.totalSecondsCounter2 = 0;
+        TotalTimeCounter2.totalMillisecondsCounter2 = 0;
+        TotalTimeCounter3.totalMinutesCounter3 = 0;
+        TotalTimeCounter3.totalSecondsCounter3 = 0;
+        TotalTimeCounter3.totalMillisecondsCounter3 = 0;
+        //Lap Time Counter (Time Trials Mode)
+        LapsTimer.minutesCounter_ = 0;
+        LapsTimer.secondsCounter_ = 0;
+        LapsTimer.millisecondsCounter_ = 0;
+        LapsTimerII.minutesCounter_ = 0;
+        LapsTimerII.secondsCounter_ = 0;
+        LapsTimerII.millisecondsCounter_ = 0;
+        LapsTimerIII.minutesCounter_ = 0;
+        LapsTimerIII.secondsCounter_ = 0;
+        LapsTimerIII.millisecondsCounter_ = 0;
+        //Lap Time Counter (Split_Screen Multiplayer Mode)
+        LapsTimerMultiplayer.minutesCounter_ = 0;
+        LapsTimerMultiplayer.secondsCounter_ = 0;
+        LapsTimerMultiplayer.millisecondsCounter_ = 0;
+        LapsTimerMultiplayerII.minutesCounter_ = 0;
+        LapsTimerMultiplayerII.secondsCounter_ = 0;
+        LapsTimerMultiplayerII.millisecondsCounter_ = 0;
+        LapsTimerMultiplayerIII.minutesCounter_ = 0;
+        LapsTimerMultiplayerIII.secondsCounter_ = 0;
+        LapsTimerMultiplayerIII.millisecondsCounter_ = 0;
     }
 
     public void ResumeGame() // Close the pause menu and resume the game
@@ -22,6 +54,38 @@ public class ButtonsManager : MonoBehaviour
     {
         Time.timeScale = 1f; // 1f - Normal Time
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        // Resetting the times on the laps (except the one which are intentionally saved)
+        //Total Time Counters (Time Trials Mode)
+        TotalTimeCounter.totalMinutesCounter = 0;
+        TotalTimeCounter.totalSecondsCounter = 0;
+        TotalTimeCounter.totalMillisecondsCounter = 0;
+        TotalTimeCounter2.totalMinutesCounter2 = 0;
+        TotalTimeCounter2.totalSecondsCounter2 = 0;
+        TotalTimeCounter2.totalMillisecondsCounter2 = 0;
+        TotalTimeCounter3.totalMinutesCounter3 = 0;
+        TotalTimeCounter3.totalSecondsCounter3 = 0;
+        TotalTimeCounter3.totalMillisecondsCounter3 = 0;
+        //Lap Time Counter (Time Trials Mode)
+        LapsTimer.minutesCounter_ = 0;
+        LapsTimer.secondsCounter_ = 0;
+        LapsTimer.millisecondsCounter_ = 0;
+        LapsTimerII.minutesCounter_ = 0;
+        LapsTimerII.secondsCounter_ = 0;
+        LapsTimerII.millisecondsCounter_ = 0;
+        LapsTimerIII.minutesCounter_ = 0;
+        LapsTimerIII.secondsCounter_ = 0;
+        LapsTimerIII.millisecondsCounter_ = 0;
+        //Lap Time Counter (Split_Screen Multiplayer Mode)
+        LapsTimerMultiplayer.minutesCounter_ = 0;
+        LapsTimerMultiplayer.secondsCounter_ = 0;
+        LapsTimerMultiplayer.millisecondsCounter_ = 0;
+        LapsTimerMultiplayerII.minutesCounter_ = 0;
+        LapsTimerMultiplayerII.secondsCounter_ = 0;
+        LapsTimerMultiplayerII.millisecondsCounter_ = 0;
+        LapsTimerMultiplayerIII.minutesCounter_ = 0;
+        LapsTimerMultiplayerIII.secondsCounter_ = 0;
+        LapsTimerMultiplayerIII.millisecondsCounter_ = 0;
     }
 
     // Tracks (and Modes) Buttons that will give to the players access to them
